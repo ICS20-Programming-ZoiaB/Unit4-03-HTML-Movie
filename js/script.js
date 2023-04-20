d36b630d0b45214ba46547c625498b47b3f800ee
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-04-20 01:21:15 +0000

    Fixed variables

--- js/script.js
+++ js/script.js
@@ -18,7 +18,12 @@ function ageSent() {
  const THIRD_LIMIT = 5;
   
  //first if statement: if user age is greater than or equal to 17 years old
- if (age >= FIRST_LIMIT) {
+ if (age <= 0) {
+   message = "Please enter a valid age";
+ }
+
+ //elseif, if user age is greater than or equal to 17 years old
+ else if (age >= FIRST_LIMIT) {
    message = "You are old enough to see an R-rated movie by yourself, without a parent or guardian.";
  }
 
@@ -32,15 +37,15 @@ function ageSent() {
    message = "You can see a G or PG movie by yourself, without a parent or guardian.";
  }
 
- //elseif, if age is greater than or equal to 5
- else if (age == "") {
-   message = "Please enter age.";
- }
-
- //else (any age under 5)
- else {
+//else (any age under 5)
+  else if (age <5) {
    message = "You should be supervised by a parent or guardian while watching any movie.";
  }
+
+  //else (if age is not entered)
+  else {
+    message = "Please enter age.";
+  }
 
  //display results
   document.getElementById("results").innerHTML = message;
